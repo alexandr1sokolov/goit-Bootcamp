d4d5815b22aab6274b9d822570245ab3c1e4130e
Sokolov Alexandr <sokolov1alexandr@gmail.com>
2018-07-12 22:14:42 +0300

    hw-8 update

--- Java-Script/JS-08/hw-8/js/index.js
+++ Java-Script/JS-08/hw-8/js/index.js
@@ -1,5 +1,5 @@
 const galleryItems = [
-    { preview: 'img/preview-1.jpeg', fullview: 'img/fullview-1.jpeg', alt: "alt text 1" },
+    { preview: 'img/preview-1.jpeg', fullview: 'img/fullview-1.jpg', alt: "alt text 1" },
     { preview: 'img/preview-2.jpg', fullview: 'img/fullview-2.jpg', alt: "alt text 2" },
     { preview: 'img/preview-3.jpg', fullview: 'img/fullview-3.jpg', alt: "alt text 3" },
     { preview: 'img/preview-4.jpg', fullview: 'img/fullview-4.jpg', alt: "alt text 4" },
@@ -12,7 +12,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const fullview = document.querySelector('.fullview');
     const gallery = document.querySelector(".js-image-gallery");
 
-
     const createFullView =({preview, fullview, alt}) => `<div class="fullview-item"><img class="fullview-img" src="${fullview}" alt="${alt}"></div>`;
     fullview.insertAdjacentHTML('afterbegin',createFullView(galleryItems[0]));
 
@@ -20,31 +19,25 @@ document.addEventListener("DOMContentLoaded", () => {
     const createPreviewGallery = arr => arr.reduce((acc, preview) => acc + createPreview(preview),``);
     preview.insertAdjacentHTML('afterbegin',createPreviewGallery(galleryItems));
 
-
     const previewImgs = gallery.querySelectorAll(".preview-img");
-    const previewActiveImg = previewImgs[0].classList.add("preview-img-active");
+    previewImgs[0].classList.add("preview-img-active");
+
+    function onImageGalleryClick(event) {
+        const hasClass = event.target.classList.contains("preview-img");
+        if (!hasClass) return;
+
+        const activeFullViewImage = gallery.querySelector(".fullview-img");
+        activeFullViewImage.setAttribute("src", event.target.dataset.fullview);
+
+        setActivePreviewImg(previewImgs, event.target);
+    }
 
     gallery.addEventListener("click", onImageGalleryClick);
 
-    function onImageGalleryClick({ target }) {
-        const hasClass = target.classList.contains("preview-img");
-        if (!hasClass) return;
-        const activeFullviewImage = gallery.querySelector(".fullview-img");
-        activeFullviewImage.setAttribute("src", target.dataset.fullview);
-
-        setActivePreviewImg(previewImgs, target);
-    }
     function setActivePreviewImg(previewImgs, target) {
-        previewImgs.forEach(previewImg => {
-            if (previewImg !== target) {
-                previewImg.classList.remove("preview-img-active");
-            } else {
-                previewImg.classList.add("preview-img-active");
-            }
-        });
+        previewImgs.forEach(previewImg => (previewImg !== target)? previewImg.classList.remove("preview-img-active") : previewImg.classList.add("preview-img-active"));
     }
 });
-
 
 
 
