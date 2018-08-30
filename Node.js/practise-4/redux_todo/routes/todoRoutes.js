0a288f78bded79c906f375a791982baa2ee807a5
Sokolov Alexandr <sokolov1alexandr@gmail.com>
2018-08-30 21:46:16 +0300

    routes fixed

--- Node.js/practise-4/redux_todo/routes/todoRoutes.js
+++ Node.js/practise-4/redux_todo/routes/todoRoutes.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = express.Router();
 const todoControllers = require('../controllers/todoControllers');
 
-router.get('/tasks', todoControllers.getTasks);
+router.get('/', todoControllers.getTasks);
 
-router.get('/tasks/:id', todoControllers.getTask);
+router.get('/:id', todoControllers.getTask);
 
-router.post('/tasks', todoControllers.addTask);
+router.post('/', todoControllers.addTask);
 
-router.put('/tasks/:id', todoControllers.editTask);
+router.put('/:id', todoControllers.editTask);
 
-router.delete('/tasks/:id', todoControllers.deleteTask);
+router.delete('/:id', todoControllers.deleteTask);
 
 module.exports = router;
