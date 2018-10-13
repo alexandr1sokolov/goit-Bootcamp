08e2294c88e6546a7b95fb6cd92df3146b4a8020
Sokolov Alexandr <sokolov1alexandr@gmail.com>
2018-10-13 11:42:30 +0300

    clean start

--- React.native/practise2/my-app/index.js
+++ React.native/practise2/my-app/index.js
@@ -1,3 +1,6 @@
 import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux';
 import App from './App';
+import configureStore from './src/store/configureStore';
+
 AppRegistry.registerComponent('myapp', () => App);
