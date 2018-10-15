7b4092b284388565d22f94ef76d5b526700f9e2b
Sokolov Alexandr <sokolov1alexandr@gmail.com>
2018-10-15 15:01:58 +0300

    button changed to TouchableHighlight because of react-native 0.57.3 BUG

--- React.native/udemy/AwesomeProject/App.js
+++ React.native/udemy/AwesomeProject/App.js
@@ -1,31 +1,84 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TextInput} from 'react-native';
+import React, { Component } from "react";
+import {StyleSheet, Text, View, TextInput, TouchableHighlight} from "react-native";
+
+// import ListItem from './src/components/ListItem/ListItem';
 
 export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <TextInput/>
-      </View>
-    );
-  }
+    state = {
+        placeName: "",
+        places: []
+    };
+
+    placeNameChangedHandler = val => {
+        this.setState({
+            placeName: val
+        });
+    };
+
+    placeSubmitHandler = () => {
+        if (this.state.placeName.trim() === "") {
+            return;
+        }
+
+        this.setState(prevState => {
+            return {
+                places: prevState.places.concat(prevState.placeName)
+            };
+        });
+    };
+
+    render() {
+        // const placesOutput = this.state.places.map((place, i) => (
+        //     <ListItem key={i} placeName={place} />
+        // ));
+        return (
+            <View style={styles.container}>
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        placeholder="An awesome place"
+                        value={this.state.placeName}
+                        onChangeText={this.placeNameChangedHandler}
+                        style={styles.placeInput}
+                    />
+                    <TouchableHighlight
+                        style={styles.placeButton}
+                        onPress={this.placeSubmitHandler}
+                    >
+                        <Text> Add </Text>
+                    </TouchableHighlight>
+                </View>
+                {/*<View style={styles.listContainer}>{placesOutput}</View>*/}
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        padding: 26,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "flex-start"
+    },
+    inputContainer: {
+        // flex: 1,
+        width: "100%",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center"
+    },
+    placeInput: {
+        width: "70%"
+    },
+    placeButton: {
+        width: "30%",
+        alignItems: 'center',
+        backgroundColor: '#DDDDDD',
+        padding: 10
+    },
+    listContainer: {
+        width: "100%"
+    }
 });
+
