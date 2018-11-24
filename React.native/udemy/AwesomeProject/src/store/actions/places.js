996e7eccd7e05a87668acb16f60511efd98cb77c
Sokolov Alexandr <sokolov1alexandr@gmail.com>
2018-11-24 12:29:28 +0200

    post fetch to firebase added

--- React.native/udemy/AwesomeProject/src/store/actions/places.js
+++ React.native/udemy/AwesomeProject/src/store/actions/places.js
@@ -1,11 +1,20 @@
 import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
 
 export const addPlace = (placeName, location, image) => {
-  return {
-    type: ADD_PLACE,
-    placeName: placeName,
-    location: location,
-    image: image
+  return dispatch => {
+    const placeData = {
+      name: placeName,
+      location: location
+    };
+    fetch("https://awesome-places-223109.firebaseio.com/places.json", {
+      method: "POST",
+      body: JSON.stringify(placeData)
+    })
+      .then(res => res.json())
+      .then(parsedRes => {
+        console.log(parsedRes);
+      })
+      .catch(err => console.log(err));
   };
 };
 
