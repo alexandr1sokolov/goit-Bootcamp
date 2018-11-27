2bcc11df0fcb3dee3256277781d77a1cf38f9b4a
Sokolov Alexandr <sokolov1alexandr@gmail.com>
2018-11-27 15:44:31 +0200

    image saved to firebase and url returned

--- React.native/udemy/AwesomeProject/src/store/actions/places.js
+++ React.native/udemy/AwesomeProject/src/store/actions/places.js
@@ -2,21 +2,31 @@ import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
 
 export const addPlace = (placeName, location, image) => {
   return dispatch => {
-    const placeData = {
-      name: placeName,
-      location: location
-    };
-    fetch("https://awesome-places-223109.firebaseio.com/places.json", {
+    fetch("https://us-central1-awesome-places-223109.cloudfunctions.net/storeImage", {
       method: "POST",
-      body: JSON.stringify(placeData)
+      body: JSON.stringify({
+        image: image.base64
+      })
     })
+        .then(res=>res.json())
+        .then(parsedRes=>{
+          const placeData = {
+            name: placeName,
+            location: location,
+            image: parsedRes.imageUrl
+          };
+          return fetch("https://awesome-places-223109.firebaseio.com/places.json", {
+            method: "POST",
+            body: JSON.stringify(placeData)
+          });
+        })
       .then(res => res.json())
       .then(parsedRes => {
         console.log(parsedRes);
       })
       .catch(err => console.log(err));
+    };
   };
-};
 
 export const deletePlace = (key) => {
   return {
